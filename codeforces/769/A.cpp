5381eccece0f3ffb0c05370ee6e09af19497e3f6
Bipin Pathak <bipipathak1411@gmail.com>
2020-08-11 12:21:00 +0530

    Add solution for problem `A - Year of University Entrance` Link: https://codeforces.com/contest/769/problem/A Tags: *special problem, implementation, sortings, *800 Ref: https://codeforces.com/contest/769/submission/89555030

---
+++ codeforces/769/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  sort(a.begin(), a.end());
+  cout << a[(int)a.size() / 2];
+  return 0;
+}
