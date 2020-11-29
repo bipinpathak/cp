e59bc46df061843bdc37bd29a4892210bf24bd76
bipinpathak <bipinpathak1411@gmail.com>
2020-11-29 11:34:00 +0530

    Add solution for problem `D - Extreme Subtraction` Link: https://codeforces.com/contest/1443/problem/D Tags: dp, greedy, math, *1800 Ref: https://codeforces.com/contest/1443/submission/99835337

---
+++ codeforces/1443/D.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+vector<ll> fac;
+ll p=1e9+7;
+ll gcd(ll x, ll y) {
+	if(x==0) {
+		return y;
+	}
+	return gcd(y%x, x);
+}
+ll power(ll a, ll x) {
+	ll ans=1;
+	while(x) {
+		if(x&1) {
+			ans=(ans*a)%p;
+		}
+		x=x>>1;
+		a=(a*a)%p;
+	}
+	return ans;
+}
+ll modInv(ll a) {
+	return power(a, p-2);
+}
+ll ncr(ll n, ll r) {
+	ll ans=1;	
+	if(r!=0 && r!=n) {
+		ans=fac[n]*modInv(fac[r])%p*modInv(fac[n-r])%p;
+	}
+	return ans;
+}
+void pre(int n) {
+	ll x=1;
+	fac.push_back(x);
+	for(ll i=1; i<=n; i++) {
+		x=(x*i)%p;
+		fac.push_back(x);
+	}
+	return;
+}
+void solve() {
+	ll n;
+	cin>>n;
+	vector<ll> a(n), left(n);
+	for(auto &i : a) {
+		cin>>i;
+	}
+	left[0]=a[0];
+	for(int i=1; i<n; i++) {
+		left[i]=min(a[i]-a[i-1]+left[i-1], left[i-1]);
+		if(left[i]<0) {
+			cout<<"NO"<<endl;
+			return;
+		}
+	}
+	cout<<"YES"<<endl;
+    return;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
