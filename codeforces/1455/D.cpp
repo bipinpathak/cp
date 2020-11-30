16ae31fa2bc8f6169369ef65fd224c93af81788e
bipinpathak <bipinpathak1411@gmail.com>
2020-11-30 21:45:00 +0530

    Add solution for problem `D - Sequence and Swaps` Link: https://codeforces.com/contest/1455/problem/D Tags: dp, greedy, sortings, *1600 Ref: https://codeforces.com/contest/1455/submission/100043638

---
+++ codeforces/1455/D.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+vector<ll> fac;
+ll p=1e9+7;
+ll gcd(ll x, ll y) {
+	if(x==0) {
+		return y;
+	}
+	return gcd(y%x, x);
+}
+ll power(ll a, ll x) {
+	ll ans=1;
+	while(x) {
+		if(x&1) {
+			ans=(ans*a)%p;
+		}
+		x=x>>1;
+		a=(a*a)%p;
+	}
+	return ans;
+}
+ll modInv(ll a) {
+	return power(a, p-2);
+}
+ll ncr(ll n, ll r) {
+	ll ans=1;	
+	if(r!=0 && r!=n) {
+		ans=fac[n]*modInv(fac[r])%p*modInv(fac[n-r])%p;
+	}
+	return ans;
+}
+void pre(int n) {
+	ll x=1;
+	fac.push_back(x);
+	for(ll i=1; i<=n; i++) {
+		x=(x*i)%p;
+		fac.push_back(x);
+	}
+	return;
+}
+void solve() {
+	int n, x;
+	cin>>n>>x;
+	vector<int> a(n);
+	for(auto &i : a) {
+		cin>>i;
+	}
+	ll ans=0;
+	for(int i=0; i+1<n; i++) {
+		if(a[i]<=a[i+1]) {
+			continue;
+		}
+		int j=0;
+		while(j<=i) {
+			if(a[j]>x) {
+				swap(a[j], x);
+				ans++;
+			}
+			j++;
+		}
+		for(j=0; j<=i; j++) {
+			if(a[j]>a[j+1]) {
+				cout<<-1<<endl;
+				return;
+			}
+		}
+	}
+	cout<<ans<<endl;
+    return;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
