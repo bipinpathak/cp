53431f3053b7ba924b9b69ac7fde5b6dad040fbd
Bipin Pathak <bipipathak1411@gmail.com>
2020-12-02 17:26:00 +0530

    Add solution for problem `B - Numbers Box` Link: https://codeforces.com/contest/1447/problem/B Tags: greedy, math, *1000 Ref: https://codeforces.com/contest/1447/submission/100184919

---
+++ codeforces/1447/B.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+vector<ll> fac;
+ll p=1e9+7;
+ll gcd(ll x, ll y) {
+	if(x==0) {
+		return y;
+	}
+	return gcd(y%x, x);
+}
+ll power(ll a, ll x) {
+	ll ans=1;
+	while(x) {
+		if(x&1) {
+			ans=(ans*a)%p;
+		}
+		x=x>>1;
+		a=(a*a)%p;
+	}
+	return ans;
+}
+ll modInv(ll a) {
+	return power(a, p-2);
+}
+ll ncr(ll n, ll r) {
+	ll ans=1;	
+	if(r!=0 && r!=n) {
+		ans=fac[n]*modInv(fac[r])%p*modInv(fac[n-r])%p;
+	}
+	return ans;
+}
+void pre(int n) {
+	ll x=1;
+	fac.push_back(x);
+	for(ll i=1; i<=n; i++) {
+		x=(x*i)%p;
+		fac.push_back(x);
+	}
+	return;
+}
+void solve() {
+	int n, m;
+	cin>>n>>m;
+	vector<int> a;
+	int neg=0, x;
+	for(int i=0; i<n*m; i++) {
+		cin>>x;
+		a.push_back(abs(x));
+		if(x<0) {
+			neg++;
+		}
+	}
+	neg%=2;
+	sort(a.begin(), a.end());
+	int ans=0;
+	for(int i=0; i<n*m; i++) {
+		if(i<neg) {
+			ans-=a[i];
+		} else {
+			ans+=a[i];
+		}
+	}
+	cout<<ans<<endl;
+    return;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
