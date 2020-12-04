047a94090af2e9e2781c92de236d9f5d584a286f
bipinpathak <bipinpathak1411@gmail.com>
2020-12-04 18:40:00 +0530

    Add solution for problem `A - Cancel the Trains` Link: https://codeforces.com/contest/1453/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1453/submission/100352727

---
+++ codeforces/1453/A.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+vector<ll> fac;
+ll p=1e9+7;
+ll gcd(ll x, ll y) {
+	if(x==0) {
+		return y;
+	}
+	return gcd(y%x, x);
+}
+ll power(ll a, ll x) {
+	ll ans=1;
+	while(x) {
+		if(x&1) {
+			ans=(ans*a)%p;
+		}
+		x=x>>1;
+		a=(a*a)%p;
+	}
+	return ans;
+}
+ll modInv(ll a) {
+	return power(a, p-2);
+}
+ll ncr(ll n, ll r) {
+	ll ans=1;	
+	if(r!=0 && r!=n) {
+		ans=fac[n]*modInv(fac[r])%p*modInv(fac[n-r])%p;
+	}
+	return ans;
+}
+void pre(int n) {
+	ll x=1;
+	fac.push_back(x);
+	for(ll i=1; i<=n; i++) {
+		x=(x*i)%p;
+		fac.push_back(x);
+	}
+	return;
+}
+void solve() {
+	int n, m;
+	cin>>n>>m;
+	vector<int> down(n), left(m);
+	set<int> common;
+	int ans=0;
+	for(auto &i : down) {
+		cin>>i;
+		common.insert(i);
+	}
+	for(auto &i  : left) {
+		cin>>i;
+		if(common.find(i)!=common.end()) {
+			ans++;
+		}
+	}
+	cout<<ans<<endl;
+    return;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
