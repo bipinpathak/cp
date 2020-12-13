59ec3c8f5adb8883507e13e05b43d9a270e2c0f5
Bipin Pathak <bipipathak1411@gmail.com>
2020-12-13 17:48:00 +0530

    Add solution for problem `D - Circle Game` Link: https://codeforces.com/contest/1451/problem/D Tags: games, geometry, math, *1700 Ref: https://codeforces.com/contest/1451/submission/101097062

---
+++ codeforces/1451/D.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+	ll d, k, n=0;
+	cin>>d>>k;
+	while(2*n*n*k*k<=d*d) {
+		n++;
+	}
+	n--;
+	if(k*k*(2*n*n+2*n+1)<=d*d) {
+		cout<<"Ashish"<<endl;
+	} else {
+		cout<<"Utkarsh"<<endl;
+	}
+    return;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
