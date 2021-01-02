d774ce145a34b714d56cc40f99e76a634ac32943
Bipin Pathak <bipipathak1411@gmail.com>
2021-01-02 22:31:00 +0530

    Add solution for problem `A - Ichihime and Triangle` Link: https://codeforces.com/contest/1337/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1337/submission/103047887

---
+++ codeforces/1337/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+	int a, b, c, d;
+	cin>>a>>b>>c>>d;
+	cout<<b<<" "<<c<<" "<<c<<endl;
+    return;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
