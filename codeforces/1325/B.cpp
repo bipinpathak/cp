4e75f3cb4dd5811381ab845680d5451334459885
bipinpathak <bipinpathak1411@gmail.com>
2021-01-05 14:17:00 +0530

    Add solution for problem `B - CopyCopyCopyCopyCopy` Link: https://codeforces.com/contest/1325/problem/B Tags: greedy, implementation, *800 Ref: https://codeforces.com/contest/1325/submission/103360202

---
+++ codeforces/1325/B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+	int n;
+	cin>>n;
+	set<int> have;
+	while(n--) {
+		int x;
+		cin>>x;
+		have.insert(x);
+	}
+	cout<<have.size()<<endl;
+    return;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
