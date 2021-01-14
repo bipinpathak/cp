cca2bdebce5ee6c374755e3ea9d6d8468728cb2b
bipinpathak <bipinpathak1411@gmail.com>
2021-01-14 12:26:00 +0530

    Add solution for problem `B - Bogosort` Link: https://codeforces.com/contest/1312/problem/B Tags: constructive algorithms, sortings, *1000 Ref: https://codeforces.com/contest/1312/submission/104247643

---
+++ codeforces/1312/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+	int n;
+	cin>>n;
+	vector<int> a(n);
+	for(auto &i : a) {
+		cin>>i;
+	}
+	sort(a.rbegin(), a.rend());
+	for(auto &i : a) {
+		cout<<i<<" ";
+	}
+	cout<<endl;
+    return;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
+
