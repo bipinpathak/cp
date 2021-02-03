0243c0b38c0edec2d5d53e08bd83421411a7c3b9
Bipin Pathak <bipipathak1411@gmail.com>
2021-02-03 18:28:00 +0530

    Add solution for problem `C - Beautiful Rectangle` Link: https://codeforces.com/contest/1276/problem/C Tags: brute force, combinatorics, constructive algorithms, data structures, greedy, math, *2300 Ref: https://codeforces.com/contest/1276/submission/106370229

--- codeforces/1276/C.cpp
+++ codeforces/1276/C.cpp
@@ -53,12 +53,12 @@ void solve() {
     for(int i=0; i<ans; i++) {
         grid[i%row][(i/row+i%row)%col]=elements[i];
     }
-    cout<<ans<<'\n'<<row<<" "<<col<<'\n';
+    cout<<ans<<"\n"<<row<<" "<<col<<"\n";
     for(auto &i : grid) {
         for(auto &j : i) {
             cout<<j<<" ";
         }
-        cout<<'\n';
+        cout<<"\n";
     }
     return;
 }
