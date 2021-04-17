207a94bd6ea2fce4f3703fc85a93f981154803ce
bipinpathak <bipinpathak1411@gmail.com>
2021-04-17 12:52:00 +0530

    Add solution for problem `C - Max GCD 2` Link: https://atcoder.jp/contests/jsc2021/tasks/jsc2021_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/jsc2021/submissions/21810613

---
+++ atcoder/jsc2021/C.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int a, b;
+    cin>>a>>b;
+    int ans=0;
+    for(int i=1; i<=b; i++) {
+        int low=(a+i-1)/i, high=b/i;
+        if(high>low) {
+            ans=i;
+        }
+    }       
+    cout<<ans<<endl;
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    //cin >> t;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    return 0;
+}
+
