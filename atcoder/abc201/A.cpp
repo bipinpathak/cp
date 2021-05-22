5080b6c75e2a1a0e709e8a0cdb5a28fb20644fa8
bipinpathak <bipinpathak1411@gmail.com>
2021-05-22 12:28:00 +0530

    Add solution for problem `A - Tiny Arithmetic Sequence` Link: https://atcoder.jp/contests/abc201/tasks/abc201_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc201/submissions/22783485

---
+++ atcoder/abc201/A.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+    vector<int> a(3);
+    for(auto &i : a) {
+        cin>>i;
+    }
+    sort(a.begin(), a.end());
+    string ans="No";
+    if(2*a[1]==a[0]+a[2]) {
+        ans="Yes";
+    }
+    cout<<ans<<endl;
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    auto start=clock();
+    int t = 1;
+    //cin>>t;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    double used= (double) (clock()-start);
+    used=(used*1000)/CLOCKS_PER_SEC;
+    cerr<<fixed<<setprecision(2)<<used<<" ms"<<endl;
+    return 0;
+}
+
