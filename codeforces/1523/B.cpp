5fc6f34bb9446a5cc6064937a0ede3b648437148
bipinpathak <bipinpathak1411@gmail.com>
2021-05-30 20:32:00 +0530

    Add solution for problem `B - Lord of the Values` Link: https://codeforces.com/contest/1523/problem/B Tags: constructive algorithms, *1100 Ref: https://codeforces.com/contest/1523/submission/117885030

---
+++ codeforces/1523/B.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(auto &i : a) {
+        cin>>i;
+    }
+    cout<<3*n<<endl;
+    for(int i=1; i<=n/2; i++) {
+        int front=i, back=n-i+1;
+        cout<<"1 "<<front<<" "<<back<<endl;
+        cout<<"2 "<<front<<" "<<back<<endl;
+        cout<<"1 "<<front<<" "<<back<<endl;
+        cout<<"1 "<<front<<" "<<back<<endl;
+        cout<<"2 "<<front<<" "<<back<<endl;
+        cout<<"1 "<<front<<" "<<back<<endl;
+    }
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    auto start=clock();
+    int t = 1;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    double used= (double) (clock()-start);
+    used=(used*1000)/CLOCKS_PER_SEC;
+    cerr<<fixed<<setprecision(2)<<used<<" ms"<<endl;
+    return 0;
+}
+
