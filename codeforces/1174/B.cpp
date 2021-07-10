70862fecaf5528b60c0a5832e7affa9f94b58a9f
Bipin Pathak <bipipathak1411@gmail.com>
2021-07-10 14:00:00 +0530

    Add solution for problem `B - Ehab Is an Odd Person` Link: https://codeforces.com/contest/1174/problem/B Tags: sortings, *1200 Ref: https://codeforces.com/contest/1174/submission/121885422

---
+++ codeforces/1174/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    int odd=0, even=0;
+    for(auto &i : a) {
+        cin>>i;
+        if(i&1) {
+            odd++;
+        } else {
+            even++;
+        }
+    }
+    if(even && odd) {
+        sort(a.begin(), a.end());
+    }
+    for(auto i : a) {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    auto start=clock();
+    int t = 1;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    double used= (double) (clock()-start);
+    used=(used*1000)/CLOCKS_PER_SEC;
+    cerr<<fixed<<setprecision(2)<<used<<" ms"<<endl;
+    return 0;
+}
+
