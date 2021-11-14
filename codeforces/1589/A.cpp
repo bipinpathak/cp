1edf526bded7d5f36c9550022084d5571cf01ab6
Bipin Pathak <bipipathak1411@gmail.com>
2021-11-14 11:38:00 +0530

    Add solution for problem `A - Mathematical Addition` Link: https://codeforces.com/contest/1589/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1589/submission/135343201

---
+++ codeforces/1589/A.cpp
@@ -0,0 +1,27 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+
+void solve() {
+    ll u, v;
+    cin>>u>>v;
+    cout<<u*u<<" "<<-v*v<<endl;
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    return 0;
+}
+
