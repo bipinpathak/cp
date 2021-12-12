43d5a8e458ab1cc5967379127cc97f06ec9e898d
Bipin Pathak <bipipathak1411@gmail.com>
2021-12-12 20:48:00 +0530

    Add solution for problem `A - Life of a Flower` Link: https://codeforces.com/contest/1591/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1591/submission/138869784

---
+++ codeforces/1591/A.cpp
@@ -0,0 +1,43 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(auto &i : a) {
+        cin>>i;
+    }
+    int ans=1;
+    for(int i=0; i<n; i++) {
+        if(a[i]) {
+            ans++;
+            if(i && a[i-1]) {
+                ans+=4;
+            }
+        } else if(i && !a[i-1]) {
+            ans=-1;
+            break;
+        }
+    }
+    cout<<ans<<endl;
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    return 0;
+}
+
