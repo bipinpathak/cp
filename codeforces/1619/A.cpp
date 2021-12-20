9e5909090bef4cead8accf1dc0b61a5c3856dc98
Bipin Pathak <bipipathak1411@gmail.com>
2021-12-20 20:07:00 +0530

    Add solution for problem `A - Square String?` Link: https://codeforces.com/contest/1619/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/1619/submission/140018506

---
+++ codeforces/1619/A.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+
+void solve() {
+    string s, ans="NO";
+    cin>>s;
+    int n=(int)s.size();
+    if(n%2==0) {
+        ans="YES";
+        for(int i=0; i<n/2; i++) {
+            if(s[i]!=s[i+n/2]) {
+                ans="NO";
+                break;
+            }
+        }
+    }
+    cout<<ans<<endl;
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    return 0;
+}
+
