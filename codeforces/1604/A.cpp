dc194c32268c80dbb50dd405c9e8c3c0eee16daf
Bipin Pathak <bipipathak1411@gmail.com>
2021-12-27 17:28:00 +0530

    Add solution for problem `A - Era` Link: https://codeforces.com/contest/1604/problem/A Tags: greedy, *800 Ref: https://codeforces.com/contest/1604/submission/140737695

---
+++ codeforces/1604/A.cpp
@@ -0,0 +1,39 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(auto &i : a) {
+        cin>>i;
+    }
+    int ans=0, index=0;
+    for(auto &i : a) {
+        index++;
+        if(i>index) {
+            ans+=i-index;
+            index=i;
+        }
+    }
+    cout<<ans<<endl;
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    return 0;
+}
+
