9a11d9ed93fef32bfba0c82f5fe9902b7f734e47
Bipin Pathak <bipipathak1411@gmail.com>
2022-02-08 18:01:00 +0530

    Add solution for problem `A - Coins` Link: https://codeforces.com/contest/1061/problem/A Tags: greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1061/submission/145644271

---
+++ codeforces/1061/A.cpp
@@ -0,0 +1,26 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int s, n;
+    cin>>n>>s;
+    cout<<(s+n-1)/n<<endl;
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    return 0;
+}
+
