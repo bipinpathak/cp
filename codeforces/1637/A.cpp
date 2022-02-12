5684a9498e0c9fd93e30a9ed5fd4a3a6728fcd0a
Bipin Pathak <bipipathak1411@gmail.com>
2022-02-12 20:07:00 +0530

    Add solution for problem `A - Sorting Parts` Link: https://codeforces.com/contest/1637/problem/A Tags: brute force, sortings, *800 Ref: https://codeforces.com/contest/1637/submission/146071185

---
+++ codeforces/1637/A.cpp
@@ -0,0 +1,37 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(auto &i : a) {
+        cin>>i;
+    }
+    auto b=a;
+    sort(b.begin(), b.end());
+    string ans="YES";
+    if(a==b) {
+        ans="NO";
+    }
+    cout<<ans<<endl;
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    return 0;
+}
+
