cc684b6539bbcfec5990fd5595c40d2f4f10d6b4
Bipin Pathak <bipipathak1411@gmail.com>
2022-03-04 21:16:00 +0530

    Add solution for problem `B - Quality vs Quantity` Link: https://codeforces.com/contest/1646/problem/B Tags: brute force, constructive algorithms, greedy, sortings, two pointers, *800 Ref: https://codeforces.com/contest/1646/submission/148314526

---
+++ codeforces/1646/B.cpp
@@ -0,0 +1,42 @@
+#include "bits/stdc++.h"
+using namespace std;
+using ll = long long;
+
+void solve() {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(auto &i : a) {
+        cin>>i;
+    }
+    string ans="NO";
+    sort(a.rbegin(), a.rend());
+    ll red=0, blue=a.back();
+    for(int i=0; i<n-2-i; i++) {
+        red+=a[i];
+        blue+=a[n-2-i];
+        if(red>blue) {
+            ans="YES";
+            break;
+        }
+    }
+    cout<<ans<<endl;
+    return;
+}
+
+int main() {
+#ifdef bipinpathak
+    (void)!freopen("input.txt", "r", stdin);
+    (void)!freopen("output.txt", "w", stdout);
+#endif
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin>>t;
+    for(int i=0; i<t; i++) {
+        //cout<<"Case #"<<i+1<<": ";
+        solve();
+    }
+    return 0;
+}
+
